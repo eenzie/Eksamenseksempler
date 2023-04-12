4875d0e14ed070ce94d814174c464a114aacf814
eenzie <100802909+eenzie@users.noreply.github.com>
2023-04-12 13:06:39 +0200

    Updated solution/projects

---
+++ JMT-1/2.2 Iterationer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2_Iterationer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPGAVE B
+            for (int i = -6; i < 11; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+
+            //OPGAVE B
+            for (int timer = 0; timer <= 23; timer++)
+            {
+                for (int minutter = 0; minutter <= 59; minutter++)
+                {
+                    Console.WriteLine($"Klokken er {timer.ToString("00")}:{minutter.ToString("00")}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JMT-1/2.3 Tekst-filer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3_Tekst_filer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> randomNumbers = new List<string>();
+
+
+            //Console.WriteLine($"Random number between 5 and 100 is {randomNumber}");
+
+        }
+
+
+        public static string RandomNumber()
+        {
+            var result = string.Empty;   // variable to hold the result
+            Random randomNum = new Random();   // built in auto generator (algorithm) for random numbers
+
+            while (result.Length <= 25)   // while loop to generate 9 numbers
+            {
+                var number = randomNum.Next(20, 150);   // temp variable that then 
+                result += number.ToString();   // adds the value to the result
+
+                //alternative
+                // result += rnd.Next(1, 10).ToString();
+            }
+            return result;   // when done, result is returned
+
+        }
+    }
+}
